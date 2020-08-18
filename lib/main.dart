a0b425838a609ddc537810a2c876c446ff5b4827
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-08-18 23:13:00 +0200

    mthod nextStory added to flatbutton in main.dart along setstate

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,9 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
                   },
                   color: Colors.red,
                   child: Text(
@@ -78,6 +81,9 @@ class _StoryPageState extends State<StoryPage> {
                   onPressed: () {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(2);
+                    });
                   },
                   color: Colors.blue,
                   child: Text(
