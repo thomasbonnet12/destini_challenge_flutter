835e47e845d82f61b2fada82b6075551f2658590
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2020-08-18 23:38:10 +0200

    nextStory statement corrected where unnecessary == were put by mistake

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -38,15 +38,15 @@ class StoryBrain {
   ];
 
   String getStory() {
-    return _storyData[0].storyTitle;
+    return _storyData[storyNumber].storyTitle;
   }
 
   String choice1() {
-    return _storyData[0].choice1;
+    return _storyData[storyNumber].choice1;
   }
 
   String choice2() {
-    return _storyData[0].choice2;
+    return _storyData[storyNumber].choice2;
   }
 
   int storyNumber = 0;
@@ -61,11 +61,11 @@ class StoryBrain {
     } else if (choiceNumber == 2 && storyNumber == 0) {
       storyNumber = 1;
     } else if (choiceNumber == 1 && storyNumber == 1) {
-      storyNumber == 2;
-    } else if (choiceNumber == 2 && storyNumber == 1) {
-      storyNumber == 3;
+      storyNumber = 2;
     } else if (choiceNumber == 1 && storyNumber == 2) {
-      storyNumber == 5;
+      storyNumber = 5;
+    } else if (choiceNumber == 2 && storyNumber == 1) {
+      storyNumber = 3;
     } else if (choiceNumber == 2 && storyNumber == 2) {
       storyNumber = 4;
     } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
